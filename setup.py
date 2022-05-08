cb16c48e3b6d7c0711089f7f2a43c9895cbbbfe9
tblackcat <agawchik@mail.ru>
2022-05-08 21:17:46 +0300

    Changed setup.py for pypi

--- setup.py
+++ setup.py
@@ -7,11 +7,22 @@ with open('requirements.txt') as fp:
 setup(
     name='passholder',
     version='0.0.1',
+    license='GPL-3.0',
+    description = 'This software is needed for storage passwords \
+                    in encrypted file.',
+    url = 'https://github.com/aslanchek/Passholder',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=reqs,
     entry_points={
         'console_scripts':
         ['passholder = passholder.controller:main']
-    }
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Database',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.8',
+    ]
 )
