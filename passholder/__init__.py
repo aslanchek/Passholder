cac1495376189e42d5d068b2bfe803673d370cae
aslanchek <taibov.ag@phystech.edu>
2022-05-12 02:11:28 +0300

    NameError fixed

--- passholder/__init__.py
+++ passholder/__init__.py
@@ -1,6 +1,6 @@
-from .terminal_interface import Terminal
+from passholder.terminal_interface import Terminal
 from passholder.db import DB
 from passholder.account import Account
-from .errors import *
+from passholder.errors import *
 
 name = "passholder"
